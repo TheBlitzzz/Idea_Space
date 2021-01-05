92e81819e6382a2f0ba7827387bc6d3449689711
theBlitzzz <65937529+theBlitzz@users.noreply.github.com>
2021-01-05 14:37:59 +0800

    Homepage Part 1

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:idea_space/HomePage/Homepage.dart';
 import 'package:idea_space/MindMap/MindMap.dart';
 import 'Login/Login.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: title,
       theme: theme,
-      home: Login(),
+      home: HomePage(),
     );
   }
 }
