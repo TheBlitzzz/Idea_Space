e15e823692a85204fd6498969d58650a70fe28b2
theBlitzzz <65937529+theBlitzz@users.noreply.github.com>
2022-03-21 09:19:06 +0800

    Removed unnecessary files

--- android/app/src/main/kotlin/com/example/idea_space/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.idea_space
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 
-import 'MyApp.dart';
+import 'Login/Login.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = ThemeData(
+      primarySwatch: Colors.grey,
+      brightness: Brightness.dark,
+      textSelectionTheme: TextSelectionThemeData(
+        selectionColor: Colors.grey,
+      ),
+    );
+
+    return MaterialApp(
+      theme: theme,
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => Login(),
+      },
+    );
+  }
 }
